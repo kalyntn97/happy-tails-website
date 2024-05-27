2cda2bf1fa820a3a2d41dd12c20d6893d16086c3
kalyntn97 <tranhnnguyen13@gmail.com>
2024-05-26 23:26:34 -0400

    minor dark mode fixes

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -16,11 +16,11 @@ type Props = {
 
 function FeatureCard({ card }: Props) {
   return (
-    <div key={card.heading} className='group overflow-hidden w-full h-[400px] sm:h-[500px] flex flex-col items-center justify-center sm-6 rounded-xl bg-gray-50/50 transition ease-in-out duration-300'>
+    <div key={card.heading} className='group overflow-hidden w-full h-[400px] sm:h-[500px] flex flex-col items-center justify-center sm-6 rounded-xl bg-stone-50 dark:bg-stone-600/70 shadow-lg transition ease-in-out duration-300'>
       <div className='overflow-hidden w-auto h-4/5 group-hover:rotate-12 group-hover:h-3/5 duration-300'>
         <Image src={card.image} alt={`feature ${card.heading} image`} width={500} height={500} className='h-full w-auto object-contain' />
       </div>
-      <div className='flex flex-col bg-red-100 rounded-xl w-full h-1/5 p-4 mt-auto group-hover:h-2/5 group-hover:bg-red-200 duration-300'>
+      <div className='flex flex-col bg-red-100 dark:bg-stone-950/70 rounded-xl w-full h-1/5 p-4 mt-auto group-hover:h-2/5 group-hover:bg-red-200 dark:group-hover:bg-stone-800/70 duration-300'>
         <div className='flex flex-row items-center'>
           <Image src={card.icon} alt={`feature ${card.heading} icon`} width={64} height={64} className='size-6 sm:size-8'/>
           <h3 className='sm:text-xl mb-2 ml-2 font-bold'>{card.heading}</h3>

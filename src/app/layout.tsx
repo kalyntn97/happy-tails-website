e241c5297075ab65a0dc7ab184ca620e4ff7b679
kalyntn97 <tranhnnguyen13@gmail.com>
2024-05-26 22:13:53 -0400

    fix file name issue with case-sensitivity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { mulish } from '@/app/ui/fonts'
 //components
 import ThemeProvider from '@/components/ThemeProvider'
 import Footer from '@/components/Footer'
+import TopNav from '@/components/top-nav'
 
 export const metadata = {
   title: "Happy Tails",
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${mulish.className} antialised text-orange-900 min-h-screen w-full flex flex-col bg-red-50 dark:bg-stone-950 dark:text-red-50`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
+          <TopNav />
           <main className='w-full content-container'>{children}</main>
           <Footer />
         </ThemeProvider>
